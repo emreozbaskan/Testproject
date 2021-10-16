849046b8a048dcf92af93b4a6f76596a9ec5cdcb
emreozbaskan <emreozbaskan@gmail.com>
2021-10-16 15:53:59 +0300

    Program yazıldı test yapıldı. Kullanıcdan bir sayı alındı ve sayının karesi hesaplandı.

--- ConvertionExample/ConvertionExample/Program.cs
+++ ConvertionExample/ConvertionExample/Program.cs
@@ -10,7 +10,12 @@ namespace ConvertionExample
     {
         static void Main(string[] args)
         {
-
+            //Ekrandan kullanıcıdan bir sayı alıp bu sayının karesini hesaplayan prog yazınız.
+            Console.Write("Lütfen bir sayı giriniz.");
+            int Sayi = Convert.ToInt32(Console.ReadLine());
+            int sonuc = Sayi * Sayi;
+            Console.WriteLine("Sonuç" + sonuc);
+            Console.ReadKey();
         }
     }
 }
